bd8a61da3c9fe1be6653038c4a0177afb13db1ea
KurniawanDwiYuliyanto2057051010 <kurniawandwiyuliyanto42@gmail.com>
2022-11-08 14:46:02 +0700

    commit viewakun nadaa

---
+++ app/Views/pages/viewakun.php
@@ -0,0 +1,46 @@
+<?php echo view('templates/he'); ?>
+<div class="content-wrapper mt-4">
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <!-- left column -->
+                <div class="col-md-12">
+                    <!-- general form elements -->
+                    <div class="card card-danger">
+                        <div class="card-header" style="background-color: #800000; color: white">
+                            <h3 class="card-title">Profile</h3>
+                        </div>
+                        <div class="card-body">
+                            <div class="row ">
+                                <div class="card-body box-profile">
+                                    <div class="float-left">
+                                        <div class="text-center">
+                                            <img class="profile-user-img img-fluid img-circle" src="/Assets/AdminLTE-3.2.0/dist/img/user4-128x128.jpg" alt="User profile picture">
+                                        </div>
+                                        <h3 class="profile-username text-center">Wawan</h3>
+                                        <p class="text-muted text-center">Karyawan</p>
+                                    </div>
+                                    <div class="col mr-6">
+                                        <ul class="list-group list-group-unbordered mb-3">
+                                            <li class="list-group-item">
+                                                <b>Nama Lengkap :</b> <a>Wawan Suripto</a>
+                                            </li>
+                                            <li class="list-group-item">
+                                                <b>NIK :</b> <a>123456789012</a>
+                                            </li>
+                                            <li class="list-group-item">
+                                                <b>Cabang :</b> <a>Gedong Meneng</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+<?php echo view('templates/f'); ?>
